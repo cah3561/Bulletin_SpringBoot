2da5d81c2f878a69e5058b3f703cd263a2d924b7
YoonSoo <163626140+cah3561@users.noreply.github.com>
2024-07-08 18:35:38 +0900

    upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO article(title, content) VALUES ('가가가가', '1111')
+INSERT INTO article(title, content) VALUES ('나나나나', '2222')
+INSERT INTO article(title, content) VALUES ('다다다다', '3333')
+
+INSERT INTO article(title, content) VALUES ('당신의 인생 영화는?', '댓글입니다.')
+INSERT INTO article(title, content) VALUES ('당신의 소울 푸드는?', '댓글이용')
+INSERT INTO article(title, content) VALUES ('당신의 취미는?', '댓글~~')
+
+INSERT INTO comment(article_id, nickname, body) VALUES (4, 'Park', '어벤져스 엔드게임')
+INSERT INTO comment(article_id, nickname, body) VALUES (4, 'Kim', '비포 선셋')
+INSERT INTO comment(article_id, nickname, body) VALUES (4, 'Seok', '피아니스트')
+
+INSERT INTO comment(article_id, nickname, body) VALUES (5, 'Park', '돼지 국밥')
+INSERT INTO comment(article_id, nickname, body) VALUES (5, 'Kim2', '밀면')
+INSERT INTO comment(article_id, nickname, body) VALUES (5, 'Seok', '치킨')
+
+INSERT INTO comment(article_id, nickname, body) VALUES (6, 'Park', 'Golf')
+INSERT INTO comment(article_id, nickname, body) VALUES (6, 'Kim2', 'LOL')
+INSERT INTO comment(article_id, nickname, body) VALUES (6, 'Seok', '배그')
